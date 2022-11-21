73b02999c37da2656bf967491779217c95363579
Vinícius <viniscrv@gmail.com>
2022-11-21 13:06:24 -0300

    iterando posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,73 @@
-import Header from "./components/Header"
-import Sidebar from "./components/Sidebar"
-import Post from "./components/Post"
-import "./global.css"
-import styles from "./App.module.css"
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Post from "./components/Post";
+import "./global.css";
+import styles from "./App.module.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://i.pinimg.com/564x/ce/88/d7/ce88d760477abaf73c1b241f14a4aa48.jpg",
+      name: "Vinícius Carvalho",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Lorem ipsum 👋" },
+
+      {
+        type: "paragraph",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumquenostrum, illo mollitia magni libero repellat maiores aliquam ut commodi velit suscipit qui rerum dolorum, reiciendis fuga nam amet expedita officia? 🚀",
+      },
+
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-11-21 12:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://i.pinimg.com/564x/76/c6/20/76c620c6906cee1903c191f255fc79e1.jpg",
+      name: "suicíniV",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Lorem ipsum 👋" },
+
+      {
+        type: "paragraph",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumquenostrum, illo mollitia magni libero repellat maiores aliquam ut commodi velit suscipit qui rerum dolorum, reiciendis fuga nam amet expedita officia? 🚀",
+      },
+
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-11-16 15:30:00"),
+  },
+];
 
 function App() {
-
   return (
     <div className="App">
-      <Header/>
+      <Header />
 
       <div className={styles.wrapper}>
-        <Sidebar/>
-        <Post/>
-      </div>  
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}              
+              />
+            );
+          })}
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,42 +2,44 @@ import React from "react";
 import styles from "./Post.module.css";
 import Comment from "./Comment.jsx"
 import Avatar from "./Avatar";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-const Post = () => {
+const Post = ({author, publishedAt, content}) => {
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/viniscrv.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Vinícius Carvalho</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="20 de novembro às 17:36h" dateTime="2022-11-20 17:36:00">
-          Publicado há 1h
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Lorem ipsum 👋</p>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
-          nostrum, illo mollitia magni libero repellat maiores aliquam ut
-          commodi velit suscipit qui rerum dolorum, reiciendis fuga nam amet
-          expedita officia? 🚀
-        </p>
-
-        <p>
-          👉 <a href="#">jane.design/doctorcare</a>
-        </p>
-
-        <p>
-          <a href="#">#novoprojeto</a> <a href="#"> #nlw </a>{" "}
-          <a href="#">#rocketseat</a>
-        </p>
+        { content.map(line => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>
+          } else if (line.type === "link") {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        }) }
       </div>
 
       <form className={styles.commentForm}>
