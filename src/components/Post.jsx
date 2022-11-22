d4ee7469dc2d84c77a3786df80ac93df41db3076
Vinícius <viniscrv@gmail.com>
2022-11-22 14:07:56 -0300

    validação de comentário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import styles from "./Post.module.css";
-import Comment from "./Comment.jsx"
+import Comment from "./Comment.jsx";
 import Avatar from "./Avatar";
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 
-const Post = ({author, publishedAt, content}) => {
-
-  const [comments, setComments] = useState([
-    "interessante 🦆"
-  ]);
+const Post = ({ author, publishedAt, content }) => {
+  const [comments, setComments] = useState(["interessante 🦆"]);
 
   const [newCommentText, setNewCommentText] = useState("");
 
-  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-    locale: ptBR,
-  });
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
@@ -29,17 +30,25 @@ const Post = ({author, publishedAt, content}) => {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Este campo é obrigatório!")
+  }
+
   function deleteComment(commentToDelete) {
-    
-    const commentsWithoutDeletedOne = comments.filter(comment => {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
       return comment !== commentToDelete;
     });
 
-    confirm("Deletar comentário?") ? setComments(commentsWithoutDeletedOne) : null;
+    confirm("Deletar comentário?")
+      ? setComments(commentsWithoutDeletedOne)
+      : null;
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -52,32 +61,52 @@ const Post = ({author, publishedAt, content}) => {
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
           {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
+        {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p key={line.content}>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
-            return <p key={line.content}><a href="#">{line.content}</a></p>
+            return (
+              <p key={line.content}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
           }
-        }) }
+        })}
       </div>
 
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea onChange={handleNewCommentChange} value={newCommentText} name="comment" placeholder="Deixe um comentário ..." />
+        <textarea
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+          name="comment"
+          placeholder="Deixe um comentário ..."
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
         <footer>
-          <button type="submit">Comentar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>Comentar</button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        { comments.map(comment => {
-          return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
