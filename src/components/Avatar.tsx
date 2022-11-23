64895a6f75566f0d5448cfbe81d75f80f8909dcb
Vinícius <viniscrv@gmail.com>
2022-11-23 02:37:12 -0300

    atualizado interface  e componente Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { ImgHTMLAttributes } from "react";
 import styles from "./Avatar.module.css";
 
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  src: string;
 }
 
-const Avatar = ({ src, hasBorder = true }: AvatarProps) => {
+const Avatar = ({ hasBorder = true, ...props}: AvatarProps) => {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
+      {...props}
     />
   );
 };
